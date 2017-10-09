80a0eb8f66912b4cb249c2a136c198c19e72f548
rcaralian <rccaralian@gmail.com>
2017-10-10 01:37:50 +0800

    Added config; added login/reg pages; added footer

---
+++ inc/footer.php
@@ -0,0 +1,4 @@
+        <script src="https://cdnjs.cloudflare.com/ajax/libs/jquery/3.2.1/jquery.min.js"></script>
+
+        <script src="https://cdnjs.cloudflare.com/ajax/libs/uikit/3.0.0-beta.30/js/uikit.min.js"></script>
+        <script src="https://cdnjs.cloudflare.com/ajax/libs/uikit/3.0.0-beta.30/js/uikit-icons.min.js"></script>
